cf6272beda768bdb58baba55da180d8af178901a
unknown <ajayrajvv@gmail.com>
2023-02-16 13:09:46 +0530

    b

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -11,36 +13,59 @@ data="Your Perfect Banking Partner"
 
 inputplaceholder="Account Number"
 
-acno=''   // acno:any  
+ acno=''   // acno:any  
+ psw=''
+ 
 
-psw=''
-
-userDetails:any={
-  1000:{acno:1000,username:"anu",password:"abc123",balance:0},
-  1001:{acno:1001,username:"amal",password:"abc123",balance:0},
-  1002:{acno:1002,username:"arun",password:"abc123",balance:0},
-  1003:{acno:1003,username:"akil",password:"abc123",balance:0},
-}
-
-constructor() { }
+constructor(private router:Router,private ds:DataService) { }
 
 ngOnInit(): void {
   
 }
 
-login(){
-  alert('login clicked')
+ login(){
+   var acnum=this.acno
+   var psw=this.psw
+  const result=this.ds.login(acnum,psw)
+  if(result){
+    alert('login success')
+    this.router.navigateByUrl('dashboard')
+  } 
+  else{
+  alert("incorrect acno or password")
+}
 }
 
-acnoChange(event:any){
-  this.acno=event.target.value
-  console.log(this.acno);
-}
+// login(a:any,b:any){
+// // console.log(a.value);
+// 
+//    var acnum=a.value
+//    var psw=b.value
+//    var userDetails=this.userDetails
+//    if(acnum in userDetails){
+//    if(psw==userDetails[acnum]["password"]){
+//    alert("Login success")
+//  }
+//  else{
+//    alert("Incorrect password")
+//  }
+//    }
+//    else{
+//      alert("Acno incorrect or Not registered yrt")
+//    }
+//      alert('login clicked')
+// }
+// 
 
-pswrdChange(event:any){
-  this.psw=event.target.value
-  console.log(this.psw);
-  
-}
+//acnoChange(event:any){
+//  this.acno=event.target.value
+//  // console.log(this.acno);
+//}
+
+//pswrdChange(event:any){
+//  this.psw=event.target.value
+//  // console.log(this.psw);
+//  
+//}
 
 }
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  uname=''
+  acno=''
+  psw=''
+
+  constructor(private ds:DataService,private router:Router ) { }
+
+  ngOnInit(): void {
+  }
+
+  register(){
+    
+    var uname=this.uname
+    var acno=this.acno
+    var psw=this.psw
+
+   const result=this.ds.register(uname,acno,psw)
+
+   if(result){
+    alert('registered')
+    this.router.navigateByUrl("")
+   }
+   else{
+    alert('acno already present')
+   }
+   // console.log(uname,acno,psw);
+  }
+}
